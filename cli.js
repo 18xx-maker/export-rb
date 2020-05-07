0b182e18e1fcfa5c5a4ff33256cd5290ff754cc7
Christopher Giroir <kelsin@valefor.com>
2020-05-06 21:35:15 -0700

    feat: ability to use cli.js

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@
 // const path = require("path");
 const pkg = require("./package.json");
 const { program } = require("commander");
-const exportRb = require("./index");
+const exportRb = require("./index.games");
 
 // Global program options
 program.version(pkg.version, "-v, --version", "output the current version");
@@ -15,7 +15,7 @@ program
   .command("tiles")
   .option("-o, --output <output>", "Output to a file, defaults to STDOUT", "-")
   .action(({ output }) => {
-    console.log(exportRb.renderTiles());
+    console.log(exportRb.renderTiles(exportRb.tiles));
   });
 
 // Exports games
@@ -23,7 +23,7 @@ program
   .command("game <game>")
   .option("-o, --output <output>", "Output to a file, defaults to STDOUT", "-")
   .action((game) => {
-    console.log(exportRb.renderGame(game));
+    console.log(exportRb.renderGame(exportRb.games[game]));
   });
 
 program.parse();
